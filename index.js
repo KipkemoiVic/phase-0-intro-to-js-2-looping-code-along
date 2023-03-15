532a62ebd77673b6f757481640458f45c69ec0c2
VICTOR MITINGI <mitingi.victor@gmail.com>
2023-03-15 12:46:00 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+// Define the writeCards function
+function writeCards(namesArray, event) {
+    const messages = [];
+    for (let i = 0; i < namesArray.length; i++) {
+      messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
+  
+  // Define the countDown function
+  function countDown(number) {
+    while (number >= 0) {
+      console.log(number);
+      number--;
+    }
+  }
+  
